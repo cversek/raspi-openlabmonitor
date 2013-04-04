2e9d092f317a4166c5c51d6fb03555a18e2acb01
cversek <cversek+github@gmail.com>
2013-04-04 11:26:57 +0000

    pull more stuff into main namespace

--- DHT/__init__.py
+++ DHT/__init__.py
@@ -1,1 +1,6 @@
+from lib import DHTLibrary
 from dht22_class import DHT22
+DEFAULT_PINMODE = "BCM" #use Broadcom SoC's numbering scheme, this best matches Adafruit's Pi Cobbler pinout
+
+def setup(pinmode = DEFAULT_PINMODE):
+    DHTLibrary.setup(pinmode=pinmode)
